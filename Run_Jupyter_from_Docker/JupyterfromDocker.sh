dc47c8d079cde62d0eae999dcb600381135bb526
Dhananjay <dkumar217@gmail.com>
2019-02-10 21:31:36 -0500

    Add files via upload

---
+++ Run_Jupyter_from_Docker/JupyterfromDocker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Start CUDA Docker Script
+
+apt-get update
+apt-get upgrade
+apt-get install curl
+apt-get install python
+apt-get install python3.7
+ln -s /usr/bin/python3.7 /usr/bin/python3
+apt-get install python3-distutils 
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py 
+python3 get-pip.py
+apt-get install jupyter
+jupyter notebook --ip 0.0.0.0 --no-browser --allow-root 
